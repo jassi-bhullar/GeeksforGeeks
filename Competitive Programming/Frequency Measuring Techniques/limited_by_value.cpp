61b87b01f1872510d601c9423c907d9e64d81deb
Jashan Bhullar <jashanbhullar376@gmail.com>
2018-09-03 17:12:51 +0530

    Optimised: Make array of the size equal to largset element

---
+++ Competitive Programming/Frequency Measuring Techniques/limited_by_value.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void counFreq(int arr[], int n, int limit)
+{
+    vector<int> count(limit+1, false);
+
+    
+    for(int i = 0; i < n; i++)
+    {
+        count[arr[i]]++;
+    }
+
+    
+    for(int i = 0; i <= limit; i++)
+    {
+        if(count[i] > 0)
+            cout << i << " " << count[i] << endl;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[] = {5, 5, 6, 6, 5, 6, 1, 2, 3, 10, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int limit = 10;
+    counFreq(arr, n, limit);
+
+    return 0;
+}
